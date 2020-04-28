6816bef0742e6342ad16a3146bff6c88501acb91
JomanTheOne <63939173+JomanTheOne@users.noreply.github.com>
2020-04-27 22:17:24 -0400

    Added documents

---
+++ FinalProject/src/infected/CountryView.java
@@ -0,0 +1,179 @@
+package infected;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import javafx.scene.control.TableRow;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.Set;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceDialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+
+public class CountryView {
+		private ArrayList<Country> countries;
+		private World world;
+		private final BorderPane rootPane;
+	    TableView<Country> table;
+	    private TableColumn<Country, String> colName;
+	    private TableColumn<Country, Integer> colHealth, colInfected, colDeceased;
+	    private Label dayLabel;
+	    private long goal; // this determines which goal you're at to upgrade the symptoms
+	    HashMap<String, Symptom> symptomMap; //symptommap will make sure that no duplicate symptoms are chosen
+	    
+	    /*
+	     * ---Button Action---
+	     * Button from main leads to this method where it loads the next scene
+	     */
+	    public CountryView(World world, HashMap<String, Symptom> symptomMap) {
+	    	//Create 
+	    	this.world = world;
+	    	this.symptomMap = symptomMap;
+	    	this.goal = 5000000;
+	    	symptomMap.put(world.getVirus().getName(), world.getVirus().getSymptoms().get(0));
+	    	this.countries = world.getCountries();
+	        // name column
+	        colName = new TableColumn<>("Country Name");
+	        colName.setMinWidth(250);
+	        // create a new cellvaluefactory that gets the name property from the country object
+	        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+	        
+	        // healthy population column
+	        colHealth = new TableColumn<>("Healthy");
+	        colHealth.setMinWidth(200);
+	         // create a new cellvaluefactory that gets the healthy property from the country object
+	        colHealth.setCellValueFactory(new PropertyValueFactory<>("healthy"));
+	        
+	        // infected population column
+	        colInfected = new TableColumn<>("Infected");
+	        colInfected.setMinWidth(200);
+	        // create a new cellvaluefactory that gets the infected property from the country object
+	        colInfected.setCellValueFactory(new PropertyValueFactory<>("infected"));
+	        
+	        // deceased population column
+	        colDeceased = new TableColumn<>("Dead");
+	        colDeceased.setMinWidth(200);
+	        // create a new cellvaluefactory that gets the deceased property from the country object
+	        colDeceased.setCellValueFactory(new PropertyValueFactory<>("deceased"));
+	        
+	    	// create new table with values from the country objects
+	        table = new TableView<Country>();
+	        // Create a row factory to determine if the priority is high or low 	        
+	        
+	        
+	        // add all columns to the table
+	        table.getColumns().addAll(colName, colHealth, colInfected, colDeceased);
+	        
+	        table.setRowFactory(row -> new TableRow<Country>() {
+	            @Override
+	            public void updateItem(Country item, boolean empty) {
+	                super.updateItem(item, empty);
+	                if (item != null)
+	                {
+	                	if (item.getInfected() > 1)
+	                	{
+		                	// convert percentage to a number between 0-255 since styling uses hex
+		                	int severity = (item.getInfected()  / item.getPop() * 255);  
+		                	// left shift 16 bits in order to make the rgb hex representation
+		                	severity = severity << 16;
+		                	setStyle("-fx-background-color: #" + String.format("%04X", severity) + ";");
+	                	}
+	                }
+	                else
+	                {
+	                	// default style
+	                	setStyle("");
+	                }
+	            }
+	        });
+	        
+	        // add all the countries 
+	        for (Country c: countries)
+	        {
+	        	table.getItems().add(c);
+	        }
+	      //Create a HBox for the table and set its position
+	        HBox tableBox = new HBox(table);
+	        tableBox.setAlignment(Pos.CENTER);
+	                        
+	        //Label to change the day
+	        dayLabel = new Label();
+	        dayLabel.setPrefSize(70,70);
+	        updateDayLabel();
+	        // Paste the table on the stage
+	        rootPane = new BorderPane(tableBox);
+	                        
+	        //Create a button to update the info of each country by the # of days
+	        Button nextDayButton = new Button("Next Day");
+	        nextDayButton.setPrefSize(70, 70);
+	                        
+	        nextDayButton.setOnAction(e -> update());
+	        //Horizontal box to show the number of days and the button
+	        HBox hbox = new HBox(dayLabel, nextDayButton);
+	        hbox.setAlignment(Pos.CENTER);
+	                        
+	        //Place the HBox location at the bottom
+	        rootPane.setBottom(hbox);
+	        rootPane.setPrefSize(70, 70);
+	    }
+	    
+	    private void updateDayLabel() {
+	    	dayLabel.setText("Day: " + world.getDay());
+	    }
+	    
+	    public void update(){
+	    	world.incrementDay();
+	    	updateDayLabel();
+	    	// update the tableview
+	    	table.refresh();
+	    	if (world.getTotalSick() > goal)
+	    	{
+	    		Set set = symptomMap.entrySet();
+	    		Iterator iterator = set.iterator();
+	    		ArrayList<String> choices = new ArrayList<String>();
+	    		// iterate through the hashmap in order to populate the choice dialogue combo box
+	    		while (iterator.hasNext())
+	    		{
+	    			Map.Entry<String, Symptom> mentry = (Map.Entry<String, Symptom>) iterator.next();
+	    			choices.add(mentry.getKey());
+	    		}
+	    		ChoiceDialog<String> dialog = new ChoiceDialog<>(choices.get(1), choices);
+	    		dialog.setTitle("Oh no!");
+	    		dialog.setHeaderText(world.getVirus().getName() + " has infected more than " + goal + " people...\nYou can upgrade now.");
+	    		dialog.setContentText("Choose a new symptom:");
+
+	    		// Traditional way to get the response value.
+	    		Optional<String> result = dialog.showAndWait();
+	    		// if the user chose yes
+	    		if (result.isPresent()){
+	    			// get the result and use it as a key to get the virus object from the hashmap
+	    		    world.getVirus().addSymptom(symptomMap.get(result.get()));
+	    		    // remove the object selected from the hashmap
+	    		    symptomMap.remove(result.get());
+	    		}
+	    		goal *= 10; // increase the goal
+	    	}
+	    } 
+
+
+	    public Pane getRootPane() {
+	        return rootPane;
+	    }
+}
---
+++ FinalProject/src/infected/Main.java
@@ -0,0 +1,242 @@
+package infected;
+	
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+import javafx.event.ActionEvent; 
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+
+
+public class Main extends Application {
+	// Initialize objects / variables 
+    Stage window;
+    TextField nameInput;
+    Label countryLabel, symptomLabel, countryInfo, virusName;
+    ComboBox<String> symptomBox;
+    ComboBox<Country>countryBox;
+    ArrayList<Country> countries;
+    ArrayList<Symptom> symptoms;
+    Button startButton;
+    HashMap<String, Symptom> symptomMap;
+    
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+	        window = primaryStage;
+	        //Set the title of the application window
+	        window.setTitle("Virus Infection Game");
+			TilePane root = new TilePane();
+			
+			//Call "readCountries()" to read over all the countries and place them in 
+			//an arraylist
+			countries = readCountries();
+			
+			//Create a hashmap that will be able to map all the symptom objects
+			//Call "readSymptoms()" to read all the symptoms and place them in a hashmap
+			symptomMap = new HashMap<String, Symptom>();
+			readSymptoms();
+			
+			//Start Button
+			startButton = new Button("Start Game");
+			
+			//---Button Action--- -> Action after pressing button takes you to next scene
+			//////////////////////////////////////////////////////////////////////////////////////////
+			startButton.setOnAction(e -> {
+				//Create a virus with information on what the player inputted -> (Virus Name, Symptom, Country)
+				Virus virus = new Virus(nameInput.getText(), symptomMap.get(symptomBox.getValue())); 
+				
+				//remove the symptom from the hashmap, so it cannot be chosen again
+				symptomMap.remove(symptomBox.getValue()); 
+				countryBox.getValue().addSick(1);
+				
+				//Goes to the "CountryView" class to create the next scene ------->
+				CountryView view = new CountryView(new World(countries, virus), symptomMap);
+				
+				//Set the window height and width of next scene
+				window.setHeight(820);
+				window.setWidth(850);
+				
+				//Create 2nd scene
+			    primaryStage.getScene().setRoot(view.getRootPane());
+			});
+			//////////////////////////////////////////////////////////////////////////////////////////
+			
+			//Create virus name label
+			virusName = new Label("Enter virus name");
+			
+			//Textfield created with prompt text asking for name
+			nameInput = new TextField();
+			nameInput.setPromptText("Name");
+			
+			//Create symptom label for player to choose symptom and have an arraylist of symptom names
+			symptomLabel = new Label("Choose the starting symptom");
+			ArrayList<String> symptoms = new ArrayList<String>();
+			
+			// populate the combo box's array with the hashmap items
+    		Set set = symptomMap.entrySet();
+    		Iterator iterator = set.iterator();
+    		while (iterator.hasNext())
+    		{
+    			//Grab all the keys (Symptom Names) and add them to the symptom arraylist
+    			Map.Entry<String, Symptom> mentry = (Map.Entry<String, Symptom>) iterator.next();
+				symptoms.add(mentry.getKey());
+    		}
+    		//Add all the symptoms into the combobox
+			symptomBox = new ComboBox<String>(FXCollections.observableArrayList(symptoms));
+			symptomBox.setValue(symptoms.get(0));
+			
+			//Create country label and grab all the country objects and put them in the country combobox
+			countryLabel = new Label("Choose the starting country");
+			countryBox = new ComboBox<Country>(FXCollections.observableArrayList(countries));
+			
+			//---Button Action--- -> When the user changes the combo box, it will update the info on the country
+			/////////////////////////////////////
+			countryBox.setOnAction(updateInfo);  
+			/////////////////////////////////////
+			
+			//Create country label for the country info to display
+			countryInfo = new Label("Country info:");
+			
+			//update the comboboxes to the first element in the arraylist in order to show the country info label
+			countryBox.setValue(countries.get(0));
+			
+			//Create a vbox to align all the items vertically
+			VBox vBox = new VBox(virusName, nameInput, symptomLabel, symptomBox, countryLabel, countryBox, countryInfo, startButton);
+			vBox.setAlignment(Pos.CENTER);
+			VBox.setMargin(nameInput, new Insets(10, 30, 10, 30));
+			Scene scene = new Scene(vBox,300,400);
+			
+			// load the styling sheet
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			window.setScene(scene);
+			window.show();
+			
+		} catch(Exception e) {  //CATCH any exception and display it on console if error
+			e.printStackTrace();
+		}
+	}
+	/*
+	 * ---Button Action---
+	 * When player presses a country, it will lead to this method where it will
+	 * display all the info on the country picked
+	 */
+	EventHandler<ActionEvent> updateInfo = 
+            new EventHandler<ActionEvent>() { 
+      public void handle(ActionEvent e) 
+      { 
+    	  countryInfo.setText("Country info:\n" + countryBox.getValue().getInfo()); 
+      } 
+  }; 
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	
+	/*
+	 * Helper method to read all strings from a text file and return the strings from the list as a arraylist
+	 */
+	private static ArrayList<String> readNames(String fname)
+	{
+		ArrayList<String> names = new ArrayList<String>(); // Array list of all the names of the countries
+        try (Scanner fileReader = new Scanner(new File(fname))){
+            while (fileReader.hasNextLine()) // loop through the entire file
+            {
+                names.add(fileReader.nextLine());
+            }
+        }catch (FileNotFoundException e){	// CATCH the exception if the file for the countries is not found
+                e.printStackTrace();
+        }
+        return names;
+	}
+	
+	/*
+	 * Helper method to create country objects using readNames helper method
+	 */
+	private static ArrayList<Country> readCountries() 
+	{
+		// create arraylist object, which will be returned later
+		ArrayList<Country> countries = new ArrayList<Country>();
+		ArrayList<String> names = readNames("countries.txt");
+		
+		/*
+		 * this loop will loop through the names arraylist and will strip the string s by separating the commas 
+		 * and then create a country object
+		 */
+		for (String s : names)
+		{
+			//This will strip off all the commas from every string and number
+			//Strips the name off up to the next comma
+			String name = s.substring(0, s.indexOf(','));
+			
+			//Strips the population number off up to the next comma
+			s = s.substring(s.indexOf(',') + 1, s.length());
+			int population = Integer.parseInt(s.substring(0, s.indexOf(',')));
+			
+			//Strips the string of whether its a hot or cold country
+			s = s.substring(s.indexOf(',') + 1, s.length());
+			Boolean hot = s.substring(0, s.indexOf(',')).equals("Hot");
+			
+			//Strips the wealth number off for the country
+			s = s.substring(s.indexOf(',') + 1, s.length());
+			int wealth = Integer.parseInt(s);
+			
+			//Create hot or cold countries depending whether "Boolean hot" is true or false 
+			if (hot)
+			{
+				countries.add(new HotCountry(name, population, wealth));
+			}
+			else
+			{
+				countries.add(new ColdCountry(name, population, wealth));
+			}
+		}
+		return countries;
+	}
+	
+	/*
+	 * this function is the same as the one from the main class, but modifies the hashmap 
+	 */
+	private void readSymptoms() 
+	{
+		ArrayList<String> names = readNames("symptoms.txt");
+		/*
+		 * this loop will loop through the names arraylist and will strip the string s by separating the commas
+		 * and then create a symptom object
+		 */
+		for (String s : names)
+		{
+			//Strips the name off up to the next comma
+			String name = s.substring(0, s.indexOf(','));
+			
+			//Strips the infectivity number off
+			s = s.substring(s.indexOf(',') + 1, s.length());
+			int infectivity = Integer.parseInt(s.substring(0, s.indexOf(',')));
+			
+			//Strips the lethality number off
+			s = s.substring(s.indexOf(',') + 1, s.length());
+			int lethality = Integer.parseInt(s);
+			
+			symptomMap.put(name, new Symptom(name, infectivity, lethality)); // only add to the hashmap if the symptom doesn't already exist
+		}
+	}
+}
